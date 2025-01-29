ab4c54c7619f5beaedd1b34f23c64b766cd846ad
atharvx <atharvraj1925@gmail.com>
2025-01-29 22:12:06 +0530

    adding student list

--- cmd/student_api/main.go
+++ cmd/student_api/main.go
@@ -31,6 +31,8 @@ func main() {
 	router.HandleFunc("POST /api/students", student.New(storage))
 
 	router.HandleFunc("GET /api/students/{id}", student.GetById(storage))
+
+	router.HandleFunc("GET /api/students", student.GetById(storage))
 	//setup server
 	server := http.Server{
 		Addr:    cfg.Addr,
